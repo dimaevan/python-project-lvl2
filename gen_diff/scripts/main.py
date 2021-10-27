1748349a3782457dbdc0f094ee09e928716fafac
dimaevan <dimaevan@gmail.com>
2021-10-27 19:56:34 +0300

    Small fixing

--- gen_diff/scripts/main.py
+++ gen_diff/scripts/main.py
@@ -25,40 +25,31 @@ def main():
     # Получаем рабочую директорию и отсительное расположение файлов
     first_file = os.path.join(os.getcwd(), args.first_file)
     second_file = os.path.join(os.getcwd(), args.second_file)
+
+    file_1 = open_file(first_file)
+    file_2 = open_file(second_file)
+
     # Выбор форматирования
     if args.format == 'plain':
-        print(generate_diff(first_file, second_file, 'plain'))
+        print(generate_diff(file_1, file_2, 'plain'))
     elif args.format == 'json':
-        print(generate_diff(first_file, second_file, 'json'))
+        print(generate_diff(file_1, file_2, 'json'))
     else:
-        print(generate_diff(first_file, second_file))
+        print(generate_diff(file_1, file_2))
 
 
-def open_files(first, second):
-    # Получаем расширения обоих фалов
-    first_extend = str(first).split('.')[-1]
-    second_extend = str(second).split('.')[-1]
-    # Проверка,что файлы с одинаковым расширением
-    if first_extend != second_extend:
-        raise MyError("Wrong type of files")
-
-    if first_extend == 'json':
-        example = json.load(open(first))
-        compared = json.load(open(second))
-    elif first_extend in ("yaml", "yml"):
-        example = yaml.safe_load(open(first))
-        compared = yaml.safe_load(open(second))
-    else:
-        raise MyError("Wrong type of files")
-
-    return example, compared
+def open_file(file):
+    ext = str(file).split('.')[-1]
+    if ext == 'json':
+        return json.load(open(file))
+    elif ext in ("yaml", "yml"):
+        return yaml.load(open(file))
+    raise MyError("Wrong type of files")
 
 
 def generate_diff(obj1, obj2, type_format='stylish'):
-    first_dict, second_dict = open_files(obj1, obj2)
-    this_diff = parser_dict(first_dict, second_dict)
-    # 4 develop only
-    # print(this_diff)
+    this_diff = parser_dict(obj1, obj2)
+
     if type_format == "plain":
         return plain(this_diff)
     elif type_format == "json":
