060d6dc606b5ec39956b1949d683c9503e8de8be
Dima Evan <dimaevan@gmail.com>
2021-10-28 15:44:56 +0300

    Fixing shell

--- gendiff.py
+++ gendiff.py
@@ -3,7 +3,7 @@
 from gen_diff.main import generate_diff as gen, open_file as of
 
 
-def generate_diff(file1, file2, style):
+def generate_diff(file1, file2, style='stylish'):
     """  Shell   """
     return gen(of(file1), of(file2), style)
 
